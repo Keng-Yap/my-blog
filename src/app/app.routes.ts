7948f3602ab3d9d454c44a9480a2a9e030cb567f
Keng-Yap <kengyap123@gmail.com>
2025-11-04 11:35:04 +0800

    deploy blog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-
-// Components
 import { BlogComponent } from './component/blog/blog.component';
 
 export const routes: Routes = [
-  { path: '', component: BlogComponent }, // 👈 default route
-  { path: '**', redirectTo: '', pathMatch: 'full' } // fallback route
+  { path: '', component: BlogComponent }, // 👈 default homepage route
+  { path: '**', redirectTo: '', pathMatch: 'full' } // fallback
 ];
